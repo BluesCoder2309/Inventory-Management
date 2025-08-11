9b6eb80f2a38fd58589ff7e1517b9b6c421aaa94
utkarsh singh <singh.utkarsh2309@gmail.com>
2025-08-12 01:01:13 +0530

    Use ejs layouts to seperate the nav bar from the table

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from "express";
 import ProductController from "./src/controllers/product-controller.js";
 import path from "path";
+import ejsLayouts from "express-ejs-layouts"
 
 const server = express();
 
 // setup view engine settings
 server.set("view engine", "ejs");
 server.set("views", path.join(path.resolve(), "src", "views"));
+server.use(ejsLayouts);
 
-// Create an instance of the class
+// Create an instance of the product controller
 const productController = new ProductController();
 
 server.get("/", productController.getProducts);
